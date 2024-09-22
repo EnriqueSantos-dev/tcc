8fdc2a24f69c4daa7417d9982145d0eae0619b5a
Enrique Santos <santosenrique869@gmail.com>
2024-09-22 09:14:47 -0300

    refactor(nav-links): remove dashboard page link

--- src/constants/nav-links.ts
+++ src/constants/nav-links.ts
@@ -1,8 +1,7 @@
 "use client";
 import { NavItem } from "@/types";
-import { Component, Home } from "lucide-react";
+import { Component } from "lucide-react";
 
 export const navLinks: NavItem[] = [
-  { href: "/dashboard", label: "Início", icon: Home },
   { label: "Módulos", href: "/modules", icon: Component }
 ];
