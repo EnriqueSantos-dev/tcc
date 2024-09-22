12117dc41b63c5a599f59b0f0c03abb46643b8d3
Enrique Santos <santosenrique869@gmail.com>
2024-09-22 13:42:44 -0300

    refactor(db): add embeddings table and update document schema

--- src/lib/db/schemas/documents.ts
+++ src/lib/db/schemas/documents.ts
@@ -12,44 +12,31 @@ import { nanoid } from "nanoid";
 import { files } from "./files";
 import { modules } from "./modules";
 import { users } from "./users";
+import { embeddings } from "./embeddings";
 
-export const documents = pgTable(
-  "document",
-  {
-    id: varchar("id", { length: 191 })
-      .primaryKey()
-      .$defaultFn(() => nanoid()),
-    name: varchar("name", { length: 255 }).notNull(),
-    description: varchar("description", { length: 255 }),
-    ownerId: text("owner_id")
-      .references(() => users.id)
-      .notNull(),
-    moduleId: text("module_id")
-      .references(() => modules.id, {
-        onDelete: "cascade"
-      })
-      .notNull(),
-    fileId: text("file_id").references(() => files.id),
-    content: text("content").notNull(),
-    metadata: jsonb("metadata"),
-    embedding: vector("embedding", { dimensions: 1536 }).notNull(),
-    createdAt: timestamp("created_at", { mode: "string" })
-      .notNull()
-      .defaultNow(),
-    updatedAt: timestamp("updated_at", { mode: "string" })
-      .notNull()
-      .defaultNow()
-      .$onUpdateFn(() => sql`current_timestamp`)
-  },
-  (table) => ({
-    embeddingIndex: index("embeddingIndex").using(
-      "hnsw",
-      table.embedding.op("vector_cosine_ops")
-    )
-  })
-);
+export const documents = pgTable("document", {
+  id: varchar("id", { length: 191 })
+    .primaryKey()
+    .$defaultFn(() => nanoid()),
+  name: varchar("name", { length: 255 }).notNull(),
+  description: varchar("description", { length: 255 }),
+  ownerId: text("owner_id")
+    .references(() => users.id)
+    .notNull(),
+  moduleId: text("module_id")
+    .references(() => modules.id, {
+      onDelete: "cascade"
+    })
+    .notNull(),
+  fileId: text("file_id").references(() => files.id),
+  createdAt: timestamp("created_at", { mode: "string" }).notNull().defaultNow(),
+  updatedAt: timestamp("updated_at", { mode: "string" })
+    .notNull()
+    .defaultNow()
+    .$onUpdateFn(() => sql`current_timestamp`)
+});
 
-export const documentsRelations = relations(documents, ({ one }) => ({
+export const documentsRelations = relations(documents, ({ one, many }) => ({
   module: one(modules, {
     fields: [documents.moduleId],
     references: [modules.id]
@@ -57,7 +44,8 @@ export const documentsRelations = relations(documents, ({ one }) => ({
   file: one(files, {
     fields: [documents.fileId],
     references: [files.id]
-  })
+  }),
+  embeddings: many(embeddings)
 }));
 
 export type Document = InferSelectModel<typeof documents>;
---
+++ src/lib/db/schemas/embeddings.ts
@@ -0,0 +1,31 @@
+import {
+  index,
+  integer,
+  jsonb,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  varchar,
+  vector
+} from "drizzle-orm/pg-core";
+import { documents } from "./documents";
+
+export const embeddings = pgTable(
+  "embedding",
+  {
+    id: serial("id").primaryKey(),
+    content: text("content"),
+    metadata: jsonb("metadata"),
+    embedding: vector("embedding", { dimensions: 1536 }).notNull(),
+    documentId: text("document_id")
+      .notNull()
+      .references(() => documents.id)
+  },
+  (table) => ({
+    embeddingIndex: index("embeddingIndex").using(
+      "hnsw",
+      table.embedding.op("vector_cosine_ops")
+    )
+  })
+);
