1c365e061c09907baaa89d0fc56fd601d63e19ab
Enrique Santos <santosenrique869@gmail.com>
2025-01-11 15:10:18 -0300

    chore(db): remove sessions table

--- src/lib/db/schemas/index.ts
+++ src/lib/db/schemas/index.ts
@@ -1,6 +1,6 @@
-export * from "./sessions";
 export * from "./users";
 export * from "./modules";
 export * from "./documents";
 export * from "./roles-enum";
 export * from "./files";
+export * from "./embeddings";
