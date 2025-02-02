5033c284cf1fffe4f8c5eded9e5b3bcc4c73da23
Enrique Santos <santosenrique869@gmail.com>
2025-02-01 22:51:12 -0300

    feat(user-profile): use Clerk UserButton instead own button

--- src/components/user-profile.tsx
+++ src/components/user-profile.tsx
@@ -1,20 +1,34 @@
 import { UserButton } from "@clerk/nextjs";
-import { currentUser } from "@clerk/nextjs/server";
+import { Skeleton } from "./ui/skeleton";
 
 export default async function UserProfile() {
-  const user = await currentUser();
-
   return (
     <div className="flex items-center gap-3 rounded-md bg-zinc-900 p-2 transition-colors hover:bg-zinc-800">
-      <UserButton />
-      <div className="flex flex-col overflow-hidden">
-        <h4 className="truncate text-xs font-medium text-foreground">
-          {user?.firstName} {user?.lastName}
-        </h4>
-        <span className="truncate text-xs text-muted-foreground">
-          {user?.primaryEmailAddress?.emailAddress}
-        </span>
-      </div>
+      <UserButton
+        fallback={
+          <div className="flex flex-1 items-center gap-4">
+            <Skeleton className="size-7 rounded-full" />
+            <Skeleton className="h-3 w-3/5" />
+          </div>
+        }
+        showName
+        appearance={{
+          elements: {
+            rootBox: {
+              width: "100%"
+            },
+            userButtonTrigger: {
+              width: "100%"
+            },
+            userButtonBox: {
+              width: "100%",
+              color: "hsl(var(--foreground))",
+              flexDirection: "row-reverse",
+              justifyContent: "flex-end"
+            }
+          }
+        }}
+      />
     </div>
   );
 }
